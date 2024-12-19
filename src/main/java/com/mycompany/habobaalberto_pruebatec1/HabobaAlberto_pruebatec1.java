7dbe48bb20350724980e4769f408480a0733213f
Alberto Haboba <beto@hotmail.com>
2024-12-19 12:40:23 -0600

    creacion projecto maven

---
+++ src/main/java/com/mycompany/habobaalberto_pruebatec1/HabobaAlberto_pruebatec1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.habobaalberto_pruebatec1;
+
+/**
+ *
+ * @author betoh
+ */
+public class HabobaAlberto_pruebatec1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
