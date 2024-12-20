5c01cb1f1a6a297572dd0c54650b671d0ae7dd92
Alberto Haboba <beto@hotmail.com>
2024-12-19 19:45:17 -0600

    menu fin y Group id a com.softek y main en pom.xml

---
+++ src/main/java/com/softek/habobaalberto_pruebatec1/HabobaAlberto_pruebatec1.java
@@ -0,0 +1,157 @@
+package com.softek.habobaalberto_pruebatec1;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class HabobaAlberto_pruebatec1 {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        String cargo;
+        double salario;
+        boolean salir = false;
+        
+        while (!salir) {
+                System.out.println("+---------------------------------------------+");
+                System.out.println("|   B I E N V E N I D O   A   N U E S T R O   |");
+                System.out.println("|                                             |");
+                System.out.println("|             S I S T E M A  D E              |");
+                System.out.println("|                                             |   ### ##     ####   ### ###  ###  ##  ### ###  ### ###  ###  ##    ####   ### ##    ## ##    ## ##   ");
+                System.out.println("|     G E S T I O N  D E  E M P L E A D O S   |    ##  ##     ##     ##  ##    ## ##   ##  ##   ##  ##    ## ##     ##     ##  ##  ##   ##  ##   ##  ");
+                System.out.println("+---------------------------------------------+    ##  ##     ##     ##       # ## #   ##  ##   ##       # ## #     ##     ##  ##  ##   ##  ####     ");
+                System.out.println("|         1. Alta Empleado                    |    ## ##      ##     ## ##    ## ##    ##  ##   ## ##    ## ##      ##     ##  ##  ##   ##   #####   ");
+                System.out.println("|         2. Consultar Todos los Empleados    |    ##  ##     ##     ##       ##  ##   ### ##   ##       ##  ##     ##     ##  ##  ##   ##      ###  ");
+                System.out.println("|         3. Consultar un Empleado            |   ### ##     ####   ### ###  ###  ##     ##    ### ###  ###  ##    ####   ### ##    ## ##    ## ##   ");
+                System.out.println("|         4. Modificar Empleado               |");
+                System.out.println("|         5. Baja Empleado                    |");
+                System.out.println("|         6. Salir                            |");
+                System.out.println("+---------------------------------------------+");
+                System.out.print("Selecciona una opcion: ");
+                int opcion = teclado.nextInt();
+                teclado.nextLine();
+
+                switch (opcion) {
+                    case 1:
+                        //nombre = "";
+                        boolean nombreValido = false;
+                        while (!nombreValido) {
+                            System.out.print("Ingrese el nombre del empleado: ");
+                            nombre = teclado.nextLine();
+
+                            if (nombre.isEmpty()) {
+                                System.out.println("El nombre no puede estar vacío. Intente nuevamente.");
+                            } 
+                            else if (nombre.matches("^[a-zA-Z]+$")) {
+                                nombreValido = true;
+                                System.out.println("Nombre ingresado correctamente.");
+                            } else {
+                                System.out.println("El nombre solo puede incluir letras. Intente nuevamente.");
+                            }
+                        }
+
+                        //apellido = "";
+                        boolean apellidoValido = false;
+                        while (!apellidoValido) {
+                            System.out.print("Ingrese el apellido del empleado: ");
+                            apellido = teclado.nextLine();
+
+                            if (apellido.isEmpty()) {
+                                System.out.println("El apellido no puede estar vacío. Intente nuevamente.");
+                            } 
+                            else if (apellido.matches("^[a-zA-Z]+$")) {
+                                apellidoValido = true;
+                                System.out.println("Apellido ingresado correctamente.");
+                            } else {
+                                System.out.println("El apellido solo puede incluir letras. Intente nuevamente.");
+                            }
+                        }
+
+                        //cargo = "";
+                        boolean cargoValido = false;
+                        while (!cargoValido) {
+                            System.out.print("Ingrese el cargo del empleado: ");
+                            cargo = teclado.nextLine();
+
+                            if (cargo.isEmpty()) {
+                                System.out.println("El cargo no puede estar vacío. Intente nuevamente.");
+                            } else {
+                                cargoValido = true;
+                                System.out.println("Cargo ingresado correctamente.");
+                            }
+                        }
+
+                        //salario = 0;
+                        boolean salarioValido = false;
+                        while (!salarioValido) {
+                            System.out.print("Ingrese el salario del empleado: ");
+                            if (teclado.hasNextDouble()) {
+                                salario = teclado.nextDouble();
+                                if (salario <= 0) {
+                                    System.out.println("El salario debe ser mayor a 0. Intente nuevamente.");
+                                } else {
+                                    salarioValido = true;
+                                    teclado.nextLine();
+                                    System.out.println("Salario ingresado correctamente.");
+                                }
+                            } else {
+                                System.out.println("El salario debe ser un número válido. Intente nuevamente.");
+                                teclado.nextLine();
+                            }
+                        }                                                
+                        boolean fechaValida = false;
+                        while (!fechaValida) {
+                            System.out.print("Ingrese la fecha de ingreso del empleado (dd-MM-yyyy): ");
+                            String fechaIngreso = teclado.nextLine();
+
+                            try {
+                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                                java.sql.Date.valueOf(LocalDate.parse(fechaIngreso, formatter));
+                                fechaValida = true;
+                                System.out.println("Fecha de ingreso ingresada correctamente.");
+                            } catch (DateTimeParseException e) {
+                                System.out.println("Formato de fecha incorrecto. Use dd-MM-yyyy. Intente nuevamente.");
+                            }
+                        }
+                        
+                        System.out.println("\n                                  ****************** E M P L E A D O  A G R E G A D O  C O N  E X I T O  *******************");
+                        break;
+                
+                    case 2:
+                        System.out.println("case 2");
+                        break;
+
+                    case 3:
+                        System.out.println("case 3");
+                        break;
+
+                    case 4:
+                        System.out.println("case 4");
+                        break;
+
+                    case 5:
+                        System.out.println("case 5");
+                        break;
+
+                    case 6:
+                        System.out.println("                                                                                                             ");
+                        System.out.println("     ###  ##    ##      ## ##   #### ##    ##              ### ##   ### ##    ## ##   ###  ##  #### ##   ## ##   ");
+                        System.out.println("      ##  ##     ##    ##   ##  # ## ##     ##              ##  ##   ##  ##  ##   ##    ## ##  # ## ##  ##   ##  ");
+                        System.out.println("      ##  ##   ## ##   ####       ##      ## ##             ##  ##   ##  ##  ##   ##   # ## #    ##     ##   ##  ");
+                        System.out.println("      ## ###   ##  ##   #####     ##      ##  ##            ##  ##   ## ##   ##   ##   ## ##     ##     ##   ##  ");
+                        System.out.println("      ##  ##   ## ###      ###    ##      ## ###            ## ##    ## ##   ##   ##   ##  ##    ##     ##   ##  ");
+                        System.out.println("      ##  ##   ##  ##  ##   ##    ##      ##  ##            ##       ##  ##  ##   ##   ##  ##    ##     ##   ##    ##  ##  ##");
+                        System.out.println("     ###  ##  ###  ##   ## ##    ####    ###  ##           ####     #### ##   ## ##   ###  ##   ####     ## ##     ##  ##  ##");
+                        System.out.println("                                                                                                                  ");
+                        salir = true;
+                        break;
+
+                    default:
+                        System.out.println("                                    *****************************  O P C I O N  I N V A L I D A  *****************************                ");              }
+            }
+    }
+}
---
+++ src/main/java/logica/Empleado.java
@@ -0,0 +1,13 @@
+package logica;
+
+import java.util.Date;
+
+public class Empleado {
+    //empleado, incluyendo nombre, apellido, cargo, salario y fecha de inicio.
+    
+    String nombre;
+    String apellido;
+    String cargo;
+    int salario;
+    Date fechaInicio;
+}
